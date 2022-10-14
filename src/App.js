56774af3ac28782d2ee66ef0b860a7463bd43bb9
gunaerode <guna.learn2016@gmail.com>
2022-10-15 00:59:30 +0530

    refactor: useNavigate() to navigate programatically :smiley: :heart_eyes:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import OrderSummary from './components/OrderSummary';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/order-summary" element={<OrderSummary />} />
       </Routes>
     </>
   );
--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -2,19 +2,16 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 
-function Home() {
+function OrderSummary() {
   const navigate = useNavigate();
   return (
     <div>
-      <h1>Home</h1>
-      <Button
-        variant="contained"
-        onClick={() => navigate('order-summary', { replace: true })}
-      >
-        Place Order
+      <h1>Order Confirmed</h1>
+      <Button variant="contained" onClick={() => navigate(-1)}>
+        Go back
       </Button>
     </div>
   );
 }
 
-export default Home;
+export default OrderSummary;
